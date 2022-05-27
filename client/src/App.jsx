e8735d0b91146d648afb0a71ed1230716656edcd
Web-dev009 <shubhopal111@gmail.com>
2022-05-28 02:39:08 +0530

    Established client-server comm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,27 @@ import Cookies from 'universal-cookie';
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css'
 
+const cookies = new Cookies();
 const apiKey = 'x58v7fmayncb';
 
 
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
+const authToken = cookies.get("token");
+
+
+
+if(authToken) { 
+  client.connectUser({
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+  }, authToken)
+}
+
 
 const App = () => {
 
